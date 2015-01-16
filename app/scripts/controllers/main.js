3341b4d97f672735de8f41a0c110a23c4d891d3c
Roy Smith <roy.smith.esq@gmail.com>
2015-01-16 18:45:48 +0000

    backend mostly done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,11 @@
 'use strict';
 var MainCtrl = (function () {
     function MainCtrl($scope) {
-        this.sig = 'MainCtrl'; // I always do this to help debugging DI
+        this.sig = 'MainCtrl'; // I always do this to help debugging DI, and as my first test
         $scope.vmm = this; // thus {{vmm.sig}} within the HTML
     }
     MainCtrl.prototype.aSimpleFunction = function () {
-        return "foo";
+        return 'foo';
     };
     MainCtrl.$inject = ['$scope']; // allows safe minification
     return MainCtrl;
